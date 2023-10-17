b773308752e332421ca1afd008656a0d4669a85a
jzsantana <juliass7187@gmail.com>
2023-10-17 10:58:56 -0300

    update

--- avancando/src/App.js
+++ avancando/src/App.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import Barco from './assets/barco.jpg'
 import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
+import CarDEtails from './components/CarDEtails';
+import ExecuteFunctions from './components/ExecuteFunctions';
 
 function App() {
+
+  //const name = "Joaquim"
+  const [userName] = useState("Maria")
+
+  const cars = [
+    {id: 1, brand: "Ferrari", color: "Yellow", newCar: true, km: 0},
+    {id: 2, brand: "Jeep", color: "Black", newCar: false, km: 15326},
+    {id: 3, brand: "Chevrolet", color: "Silver", newCar: false, km: 25},
+  ]
+
+  function showMessage(){
+    console.log("Olá eu sou")
+  }
+
   return (
     <div className="App">
       <h1>
@@ -24,6 +42,23 @@ function App() {
       <ManageData/>
       <ListRender/>
       <ConditionalRender/>
+      {/* Props */}
+      <ShowUserName name={userName}/>
+      {/* destruction */}
+      <CarDEtails brand="Audi" color="Red" km={1000} newCar={false} />
+      <CarDEtails brand="Ford" color="Black" km={1500} newCar={false} />
+      <CarDEtails brand="Volksvagen" color="Grey" km={0} newCar={true} />
+      
+      {cars.map((car) => (
+        <CarDEtails 
+            key={car.id}
+            brand={car.brand} 
+            color={car.color} 
+            km={car.km} 
+            newCar={car.newCar}/>
+      ))}
+      <ExecuteFunctions myFunction={showMessage} />
+
     </div>
   );
 }
